7f89d96526e9e76c7e3b7487856faff32ca1b2de
George Benjamin <msdcconnect@gmail.com>
2018-12-12 23:23:02 +0100

    Created book model

---
+++ models/Book.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+    },
+    author: {
+        type: String,
+        required: true,
+    }
+});
+
+const Book = mongoose.model('Books', bookSchema);
+
+module.exports.findBook = (name, callback) => {
+    Book.findOne({
+        name: name,
+    }, callback)
+}
